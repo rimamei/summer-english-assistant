f6bd73de4af7fe28a3f1ad043026e1639ab111d9
Rima <rimameih@gmail.com>
2025-11-02 19:52:08 +0700

    feat: add default value

---
+++ src/constants/language.ts
@@ -0,0 +1,1 @@
+export const languages = ['es-ES', 'id-ID', 'ja-JP', 'en-US']
---
+++ src/pages/background/index.ts
@@ -0,0 +1,31 @@
+import { languages } from "@/constants/language";
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+        const language = chrome.i18n.getUILanguage();
+
+        const defautLanguage = languages.includes(language) ? language.split('-')[0] : 'en';
+
+        const preferenceData = {
+            lang: defautLanguage,
+            theme: 'light'
+        };
+
+        const configurationData = {
+            source_lang: 'en',
+            target_lang: 'id',
+            mode: 'translation',
+            accent: 'american',
+            selector: 'context',
+            summarizer_type: 'headline',
+            summarizer_length: 'short',
+        }
+
+        chrome.storage.local.set({
+            preferences: JSON.stringify(preferenceData),
+            ext_status: false,
+            settings: JSON.stringify(configurationData),
+        });
+
+    }
+});
