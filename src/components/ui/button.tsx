b952aa506502ad73d2450a176a169f51078c8d37
Rima <rimameih@gmail.com>
2025-11-15 23:52:01 +0700

    style: replace previous color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground hover:bg-primary/80 dark:bg-accent dark:disabled:bg-zinc-500 dark:text-white dark:hover:bg-amber-700',
+          'bg-accent text-primary-foreground hover:bg-primary/80 dark:bg-accent dark:disabled:bg-zinc-500 dark:text-white',
         destructive:
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-red-600 dark:hover:bg-red-700',
         outline:
