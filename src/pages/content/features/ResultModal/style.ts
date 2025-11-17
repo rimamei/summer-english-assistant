8546d47133c2a620ad935c0cdaae16045ed09824
Rima <rimameih@gmail.com>
2025-11-17 12:19:18 +0700

    style: fix result modal style

--- src/pages/content/features/ResultModal/style.ts
+++ src/pages/content/features/ResultModal/style.ts
@@ -8,7 +8,7 @@ export const classes = {
     borderRadius: '12px',
     boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
     padding: '4px',
-    minWidth: '500px',
+    width: '250px',
     maxWidth: '400px',
     minHeight: '100px',
     zIndex: 2147483648,
